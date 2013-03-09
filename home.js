7a1f763ca0c5722d2818a7e6ea5883474ee7fc0e
spencer kelly <spencerwater@gmail.com>
2013-03-09 23:03:38 +0000

    DEAL WITH IT

--- home.js
+++ home.js
@@ -5,9 +5,13 @@ $(function(document) {
 
 	get_history(1,function(pages){
 
-  	$("#daycount").html(pages.length);
-
-  	var wakeup=pages[0].hour||8;
+  	$("#daycount").html(pages.length||"goodmorning");
+    var d=new Date();
+    var now=d.getHours();
+  	var wakeup=now;
+    if(pages.length>0){
+      wakeup=pages[0].hour
+    }
   	if(wakeup>12){
   		wakeup=wakeup-12
   		wakeup=wakeup+"pm"
